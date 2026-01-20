379f84a5c1940562b653a8f9fcaddd3d92114a04
Hoa Nguyen <hoanguyensm.cc@gmail.com>
2026-01-20 09:58:50 +0700

    add condition to check favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -6,7 +6,7 @@ import MovieCard from '../components/MovieCard';
 const Favorites = () => {
     const { favorites } = useMovieContext();
 
-    if (favorites) {
+    if (favorites.length > 0) {
         return (
             <div className="favorites">
                 <h2>Your Favorite Movies</h2>
